d3460d60047798389b86980a8d6b764b7cef3745
Rushikendre060 <113415417+Rushikendre060@users.noreply.github.com>
2025-11-13 13:26:30 +0530

    AddedRailwaysJavaClass

---
+++ src/main/java/com/example/booking_mss/Railway.java
@@ -0,0 +1,14 @@
+package com.example.booking_mss;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Railway {
+
+    @GetMapping("/Railway")
+    public String getdata() {
+        return "Railway ticket are at @35% discount ";
+    }
+
+}
